f9d0b14b8c98b6411a38b64d13861680b2248904
Timothy Palpant <tim@palpant.us>
2022-05-22 11:12:11 -0400

    Use upstream manylinux2014 images to build wheels

--- build_wheels.sh
+++ build_wheels.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -ex
 
-cd /src
+for PYBIN in /opt/python/*/bin; do
+  if [[ "$PYBIN" == *cp311* ]] || [[ "$PYBIN" == *pypy* ]]; then
+    continue
+  fi
 
-eval "$(pyenv init -)";
+  $PYBIN/pip wheel /src --no-deps -w /src/dist
+done
 
-for PYVERSION in $(pyenv versions --bare); do
-  pyenv shell $PYVERSION
-  pip install -U setuptools wheel
-  python setup.py test
-  # NB: This is not actually manylinux2010-compatible, but ABSL and c++11
-  # have ABI requirements that won't compile on the manylinux2010 images.
-  # Tensorflow images are also built on Ubuntu 14.04, this seems better
-  # than forcing everyone compile it. manylinux2014 should be coming soon (tm).
-  python setup.py sdist bdist_wheel --plat-name manylinux2010_x86_64
+for whl in /src/dist/*.whl; do
+    auditwheel repair "$whl" --plat "$PLAT" -w /src/dist
 done
--- docker_build_wheels.sh
+++ docker_build_wheels.sh
@@ -2,5 +2,5 @@
 
 set -exuo pipefail
 
-docker build -t pypprof:latest .
-docker run -it -v `pwd`:/src pypprof:latest /src/build_wheels.sh
+python3 setup.py sdist
+docker run -it -v `pwd`:/src -e PLAT=manylinux2014_x86_64 quay.io/pypa/manylinux2014_x86_64 /src/build_wheels.sh
